1a8b1f73539003234816a0e530112841f7a35440
Ignacio Aprile <ignacioaprile@hotmail.com>
2024-06-26 12:10:47 -0300

    cambiando la ruta a relativa a la raíz del servidor

--- js/models/Storage.js
+++ js/models/Storage.js
@@ -1,7 +1,7 @@
 import Producto from "./Producto.js";
 
 export default class StorageService{
-    static url = 'datos.json';
+    static url = '/json/datos.json';
 
     static setItem(key,value){
         localStorage.setItem(key,JSON.stringify(value));
